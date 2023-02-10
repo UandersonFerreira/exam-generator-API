320879648f7c836f369c71843ecc579c864428fd
UandersonFerreira <103908858+UandersonFerreira@users.noreply.github.com>
2023-02-10 15:32:41 -0300

    ApplicationUser

--- src/main/java/br/com/devdojo/examgenerator/persistence/model/ApplicationUser.java
+++ src/main/java/br/com/devdojo/examgenerator/persistence/model/ApplicationUser.java
@@ -1,53 +1,40 @@
 package br.com.devdojo.examgenerator.persistence.model;
 
-<<<<<<< HEAD
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotEmpty;
 
-@Entity
-public class ApplicationUser extends AbstractEntity {
-=======
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+public class ApplicationUser extends AbstractEntity  {
+            @NotEmpty(message = "The field username cannot be empty")
+            @Column(unique = true)
+            private String username;
+            @NotEmpty(message = "This field password cannot be empty")
+            private String password;
+            @OneToOne
+            private Professor professor;
 
-@Entity
-public class ApplicationUser extends AbstractEntity{
->>>>>>> origin/master
-    @NotEmpty(message = "The field username cannot be empty")
-    @Column(unique = true)
-    private String username;
-    @NotEmpty(message = "This field password cannot be empty")
-    private String password;
-    @OneToOne
-    private Professor professor;
+            public String getUsername() {
+                return username;
+            }
 
-<<<<<<< HEAD
-    public String getUsername() {
-        return username;
-    }
+            public void setUsername(String username) {
+                this.username = username;
+            }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+            public String getPassword() {
+                return password;
+            }
 
-    public String getPassword() {
-        return password;
-    }
+            public void setPassword(String password) {
+                this.password = password;
+            }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+            public Professor getProfessor() {
+                return professor;
+            }
 
-    public Professor getProfessor() {
-        return professor;
-    }
-
-    public void setProfessor(Professor professor) {
-        this.professor = professor;
-    }
-=======
-
->>>>>>> origin/master
+            public void setProfessor(Professor professor) {
+                this.professor = professor;
+            }
 }//class
